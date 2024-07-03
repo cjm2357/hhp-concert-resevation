ad97182c0cb96a1026347ae0dff52934cadca3ab
cjm2357 <cjm2357@gmail.com>
2024-07-03 21:44:27 +0900

    initialize

---
+++ src/main/java/com/example/concert_reservation/ConcertReservationApplication.java
@@ -0,0 +1,13 @@
+package com.example.concert_reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcertReservationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcertReservationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/concert_reservation/ConcertReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.concert_reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcertReservationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
