8ea84c722cf322cf2f87e000aa310eb097abac58
cjm2357 <cjm2357@gmail.com>
2024-07-19 02:48:48 +0900

    도메인/Entity 객체 추가

---
+++ src/main/java/com/example/concert_reservation/domain/entity/Seat.java
@@ -0,0 +1,27 @@
+package com.example.concert_reservation.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Seat {
+
+    public enum State {
+        RESERVED, EMPTY
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private Integer concertId;
+    private Integer scheduleId;
+    private Integer seatNo;
+
+    @Enumerated(EnumType.STRING)
+    private State state;
+    private Long price;
+    private String grade;
+}
---
+++ src/main/java/com/example/concert_reservation/domain/entity/Token.java
@@ -0,0 +1,55 @@
+package com.example.concert_reservation.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Token {
+    public enum TokenState {
+        ACTIVATE, WAITING, EXPIRED
+    }
+
+    public static final int AVAILABLE_NUM = 50;
+    public static final int EXPIRED_TIME_TEN_MIN = 10;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @OneToOne
+    private User user;
+
+    private UUID tokenKey;
+
+    private LocalDateTime createdTime;
+    private LocalDateTime expiredTime;
+
+    @Enumerated(EnumType.STRING)
+    private TokenState state;
+
+    @Transient
+    private Integer order;
+
+
+    @Builder
+    public Token(Integer id, User user, UUID key, LocalDateTime createdTime, LocalDateTime expiredTime, TokenState state) {
+        this.id = id;
+        this.user = user;
+        this.tokenKey = key;
+        this.createdTime = createdTime;
+        this.expiredTime = expiredTime;
+        this.state = state;
+    }
+
+}
+
+
