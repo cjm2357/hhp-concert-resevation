20c92a465a33f6624298e713f9658bd9f20c888e
cjm2357 <cjm2357@gmail.com>
2024-08-02 02:27:32 +0900

    분산환경에서 스케줄러가 한번만 작동하도록 코드수정

--- src/main/java/com/example/concert_reservation/presentation/scheduler/ReservationScheduler.java
+++ src/main/java/com/example/concert_reservation/presentation/scheduler/ReservationScheduler.java
@@ -2,19 +2,45 @@ package com.example.concert_reservation.presentation.scheduler;
 
 
 import com.example.concert_reservation.application.ConcertFacade;
+import lombok.RequiredArgsConstructor;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.util.concurrent.TimeUnit;
+
+@RequiredArgsConstructor
 @Component
 public class ReservationScheduler {
 
-    @Autowired
-    ConcertFacade concertFacade;
 
-    //30초마다 작동
-    @Scheduled(fixedRate = 30 * 1000)
+    private final ConcertFacade concertFacade;
+    private final RedissonClient redissonClient;
+
+    private final String reservationScheduleLockKey = "reservationScheduleLockKey";
+
+    //10초마다 작동
+    @Scheduled(fixedRate = 10 * 1000)
     public void expireReservation() {
+
+        // 분산환경에서 스케줄 중복실행방지를 위해 분산 락 사용
+        RLock rLock = redissonClient.getLock(reservationScheduleLockKey);
+        try {
+            boolean available = rLock.tryLock(2, 5, TimeUnit.SECONDS);
+            if (!available) {
+                throw new RuntimeException("Lock 이용불가");
+            }
+            concertFacade.expireReservation();
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Lock획득 실패");
+        } finally {
+            rLock.unlock();
+        }
+
         concertFacade.expireReservation();
     }
 }
--- src/main/java/com/example/concert_reservation/presentation/scheduler/TokenScheduler.java
+++ src/main/java/com/example/concert_reservation/presentation/scheduler/TokenScheduler.java
@@ -1,20 +1,49 @@
 package com.example.concert_reservation.presentation.scheduler;
 
 import com.example.concert_reservation.application.TokenFacade;
+import com.example.concert_reservation.domain.entity.Point;
+import lombok.RequiredArgsConstructor;
+import org.redisson.Redisson;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.redisson.client.RedisClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.util.concurrent.TimeUnit;
+
 @Component
+@RequiredArgsConstructor
 public class TokenScheduler {
 
-    @Autowired
-    TokenFacade tokenFacade;
 
-    //30초마다 작동
-    @Scheduled(fixedRate = 30 * 1000)
-    public void expireToken() {
-        tokenFacade.expireToken();
+    private final TokenFacade tokenFacade;
+    private final RedissonClient redissonClient;
+
+    private final String tokenScheduleLockKey = "tokenScheduleLock";
+    private final int activateCount = 50;
+
+    //10초마다 작동
+//    @Scheduled(fixedRate = 10 * 1000)
+    public void changeActiveStatus() {
+
+        // 분산환경에서 스케줄 중복실행방지를 위해 분산 락 사용
+        RLock rLock = redissonClient.getLock(tokenScheduleLockKey);
+        try {
+            boolean available = rLock.tryLock(2, 5, TimeUnit.SECONDS);
+            if (!available) {
+                throw new RuntimeException("Lock 이용불가");
+            }
+            tokenFacade.activateTokens(activateCount);
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Lock획득 실패");
+        } finally {
+            rLock.unlock();
+        }
+        
     }
 
 }
