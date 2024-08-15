1889b8b673ff19cedfcf097803c2d46b7ec386d5
cjm2357 <cjm2357@gmail.com>
2024-08-16 01:50:28 +0900

    보상트랜잭션을 위한 메서드 구현

--- src/main/java/com/example/concert_reservation/domain/payment/PaymentRepository.java
+++ src/main/java/com/example/concert_reservation/domain/payment/PaymentRepository.java
@@ -6,4 +6,6 @@ public interface PaymentRepository {
 
     Payment save(Payment payment);
 
+    void deleteById(Integer paymentId);
+
 }
---
+++ src/main/java/com/example/concert_reservation/infra/payment/message/PaymentState.java
@@ -0,0 +1,5 @@
+package com.example.concert_reservation.infra.payment.message;
+
+public enum PaymentState {
+    INIT, POINT, SEAT, FINISHED
+}
